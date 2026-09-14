37cd9ff3fb94cfae64d28834a223d04b99ae8740
utsav-lal-356 <utsavlala356@gmail.com>
2026-09-14 18:41:03 +0530

    Add files via upload

---
+++ 16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+printf("MARKS OF 10TH STANDARD !\n");
+printf("NAME : utsav\n");
+printf("ROLL NO. :216\n");
+printf("MARKS OF MATHS : 98\n");
+printf("MARKS OF SCIENCE :99\n");
+printf("MARKS OF ENGLISH :92\n");
+printf("MARKS OF SANSKRIT :96\n");
+printf("MARKS OF GUJARATI :94\n");
+printf("MARKS OF SOCIAL SCIENCE :96\n");
+
+    return 0;
+}
